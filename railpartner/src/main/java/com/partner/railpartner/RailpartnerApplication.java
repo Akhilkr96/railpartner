6a0f3f7b21fefdc52d44363bb0d838662fe4fc7a
pi <pi@localhost>
2022-03-04 09:19:33 +0530

    rail partner starts

---
+++ railpartner/src/main/java/com/partner/railpartner/RailpartnerApplication.java
@@ -0,0 +1,13 @@
+package com.partner.railpartner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailpartnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailpartnerApplication.class, args);
+	}
+
+}
---
+++ railpartner/src/test/java/com/partner/railpartner/RailpartnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.partner.railpartner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailpartnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
